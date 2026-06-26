f36aefb49302fe6b335c3125f63ba923f77abe91
NimaMaria <nimamariajacob@gmail.com>
2026-06-27 01:32:08 +0530

    improved card structure

--- src/Components/StudentCard.jsx
+++ src/Components/StudentCard.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 import { useState } from 'react'
 
 const StudentCard = (props) => {
+  const [likes, setLikes] = useState(0)
 
-    const[likes, setLikes] = useState(0)
-
-    function increaseLike(){
-      setLikes(likes+1)
-    }
+  function increaseLike() {
+    setLikes(likes + 1)
+  }
 
   return (
-    <>
-      <div className='card'> 
+    <div className='card'>
+      <div className='card-info'>
         <h2>{props.name}</h2>
         <p>Course: {props.course}</p>
-        <h3>Likes: {likes}</h3>
-        <button onClick={increaseLike}>Like</button>
       </div>
-    </>
+
+      <div className='card-likes'>
+        <span className='likes-count'>Likes: {likes}</span>
+        <button onClick={increaseLike} className='like-button'>Like</button>
+      </div>
+    </div>
   )
 }
 
